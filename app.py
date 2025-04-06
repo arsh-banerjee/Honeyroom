70ad90962c1f5c358bbfcab61a2033550ea3c989
Arsh Banerjee <78701372+arsh-banerjee@users.noreply.github.com>
2025-04-06 13:10:45 -0400

    Fixed bug with huggingface

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 import glob
 import streamlit as st
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import CharacterTextSplitter
